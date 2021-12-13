ed2c3c59575b31b8d02f33b239b0ed20bb930a1b
katelynjohnnson <89552485+katelynjohnnson@users.noreply.github.com>
2021-12-12 20:54:48 -0600

    django v

--- Pizzas/views.py
+++ Pizzas/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render, redirect
 from .forms import PizzaForm, ToppingForm
 from .models import Pizza, Toppings
-
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 def index(request):
     return render(request, 'Pizzas/index.html')
 
+@login_required
 def pizzas(request):
     pizzas = Pizza.objects.order_by('date')
 
     context = {'pizzas':pizzas}
     return render(request, 'pizzas/pizzas.html', context)
 
+@login_required
 def pizza(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
     toppings = pizza.toppings_set.order_by('-date_added')
@@ -21,7 +24,7 @@ def pizza(request, pizza_id):
     context = {'pizza': pizza, 'toppings': toppings, 'comments': comments}
     return render(request, 'pizzas/pizza.html', context)
 
-
+@login_required
 def new_pizza(request):
     if request.method != 'POST':
         form = PizzaForm()
@@ -39,6 +42,7 @@ def new_pizza(request):
     context = {'form':form}
     return render(request, 'pizzas/new_pizza.html', context)
 
+@login_required
 def new_topping(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
 
@@ -59,3 +63,24 @@ def new_topping(request, pizza_id):
 
     context = {'form':form, 'pizza':pizza}
     return render(request, 'pizzas/new_topping.html', context)
+
+@login_required
+def edit_topping(request, topping_id):
+    topping = Toppings.objects.get(id=topping_id)
+    pizza = topping.pizza
+
+
+    if pizza.owner != request.user:
+        raise Http404
+    if request.method != 'POST':
+        form = ToppingForm(instance=topping)
+    else:
+        form = ToppingForm(instance=topping, data=request.POST)
+
+
+        if form.is_valid():
+            form.save()
+            return redirect('pizzas:pizza', pizza_id=pizza.id)
+
+    context = {'topping':topping, 'pizza':pizza, 'form':form}
+    return render(request, 'pizzas/edit_topping.html', context)
--- Pizzeria/settings.py
+++ Pizzeria/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Pizzas',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = 'users:login'

401eb61b9dab5632956c7b35864e37eeef06e226
katelynjohnnson <89552485+katelynjohnnson@users.noreply.github.com>
2021-12-12 18:34:23 -0600

    pizzas.html

--- Pizzas/views.py
+++ Pizzas/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Pizza, Topping
+
 
 # Create your views here.
 def index(request):
     return render(request, 'Pizzas/index.html')
+
+def pizzas(request):
+    pizzas = Pizza.objects.order_by('date')
+
+    context = {'pizzas':pizzas}
+    return render(request, 'pizzas/pizzas.html', context)

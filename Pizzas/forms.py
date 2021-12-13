8658906d51d93284d4c54d709b4721082cc54765
katelynjohnnson <89552485+katelynjohnnson@users.noreply.github.com>
2021-12-12 20:17:55 -0600

    django iv

--- Pizzas/forms.py
+++ Pizzas/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Pizza
+from .models import Pizza, Toppings
 
 class PizzaForm(forms.ModelForm):
     class Meta:
         model = Pizza
+        fields = ['name']
+        labels = {'name':''}
+
+class ToppingForm(forms.ModelForm):
+    class Meta:
+        model = Toppings
         fields = ['name']
         labels = {'name':''}
--- Pizzas/views.py
+++ Pizzas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import PizzaForm
+from .forms import PizzaForm, ToppingForm
 from .models import Pizza, Toppings
 
 
@@ -38,3 +38,24 @@ def new_pizza(request):
 
     context = {'form':form}
     return render(request, 'pizzas/new_pizza.html', context)
+
+def new_topping(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+
+    if request.method != 'POST':
+        form = ToppingForm()
+
+    else:
+        form = ToppingForm(data=request.POST)
+
+        if form.is_valid():
+            new_topping = form.save(commit=False)
+            new_topping.pizza = pizza
+            new_topping.owner = request.user 
+            new_topping.save()
+
+            return redirect('pizzas:pizza', pizza_id=pizza_id)
+
+
+    context = {'form':form, 'pizza':pizza}
+    return render(request, 'pizzas/new_topping.html', context)
